d3443093f3728d21aa4d4bfac8afbd3f417bc4e5
Niraj <136625649+Niraj4444@users.noreply.github.com>
2025-08-04 10:50:06 +0000

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './Digitalbook.css';
+import './App.css';
 
 // --- Component Imports ---
 import Navbar from './components/Navbar';
